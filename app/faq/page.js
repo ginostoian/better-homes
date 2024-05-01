6010e658e64e6d506d2b4a8544078d0fd5adae63
Gino Stoian <ginostoian@gmail.com>
2024-05-01 13:20:14 +0300

    add faq copy

--- app/faq/page.js
+++ app/faq/page.js
@@ -1,10 +1,10 @@
+import ContactForm from "@/components/ContactForm";
 import FAQ from "@/components/FAQ";
 import FeaturesListicle from "@/components/FeaturesListicle";
 import Problem from "@/components/Problem";
 import Testimonials11 from "@/components/Testimonials11";
 import WithWithout from "@/components/WithWithout";
 import BlogHighlight from "@/components/blog/BlogHighlight";
-import ContentRow from "@/components/contentRow/ContentRow";
 import Hero from "@/components/hero/Hero";
 import SectionTitle from "@/components/sectionTitle/SectionTitle";
 import SocialProof from "@/components/socialProof/SocialProof";
@@ -13,54 +13,36 @@ import TextGrid from "@/components/textGrid/TextGrid";
 import config from "@/config";
 
 export default function Page() {
-  const homepageCopy = config.copy.homepage;
-  const whatWeDoCopy = config.copy.homepage.whatWeDoSection;
-  const whatWeDoArr = [
-    whatWeDoCopy.bathroom,
-    whatWeDoCopy.kitchen,
-    whatWeDoCopy.generalRenovation,
-    whatWeDoCopy.interiorDesign,
-    whatWeDoCopy.houseExtension,
-    whatWeDoCopy.smartHome,
-  ];
-  const howWeDoItCopy = config.copy.homepage.howWeDoItSection;
+  const faqPageCopy = config.copy.faqPage;
+  //   const whatWeDoCopy = aboutPageCopy.whatWeDoSection;
+  //   const whatWeDoArr = [
+  //     whatWeDoCopy.fullHome,
+  //     whatWeDoCopy.bathroomInstallation,
+  //     whatWeDoCopy.kitchenInstallation,
+  //     whatWeDoCopy.structuralWork,
+  //     whatWeDoCopy.heating,
+  //     whatWeDoCopy.flooringInstallation,
+  //   ];
+  const howWeDoItCopy = faqPageCopy.howWeDoItSection;
   const reviewsCtaCopy = config.copy.homepage.reviewsSection;
-  const faqCopy = config.copy.homepage.faqs;
-  const blogHighlightCopy = config.copy.homepage.blogHighlights;
+  const faqCopy = faqPageCopy.faqs;
+  const blogHighlightCopy = faqPageCopy.blogHighlights;
 
   return (
     <>
       <main>
         <Hero
-          title={homepageCopy.title}
-          titleAccent={homepageCopy.titleAccent}
-          subtitle={homepageCopy.subtitle}
-          heroCTA={homepageCopy.heroCTA}
-          heroImgUrl={homepageCopy.heroImgUrl}
+          title={faqPageCopy.title}
+          titleAccent={faqPageCopy.titleAccent}
+          subtitle={faqPageCopy.subtitle}
+          heroCTA={faqPageCopy.heroCTA}
+          heroImgUrl={faqPageCopy.heroImgUrl}
+          ctaTallyFormLink={faqPageCopy.ctaTallyFormLink}
         />
         <SocialProof />
+        <FAQ content={faqCopy} />
         <Problem />
         <FeaturesListicle />
-        <SectionTitle
-          title="What we do"
-          subtitle="Full service renovation company"
-        />
-        {whatWeDoArr.map((item, i) => {
-          return (
-            <ContentRow
-              key={i}
-              title={item.title}
-              subtitle={item.subtitle}
-              subtitleAccent={item.subtitleAccent}
-              p1={item.p1}
-              p2={item.p2}
-              cta={item.CTA}
-              imgSrc={item.imgSrc}
-              order={i % 2 === 0 ? false : true}
-              slug={item.slug}
-            />
-          );
-        })}
         <WithWithout />
         <SectionTitle
           title="How we do it"
@@ -69,7 +51,10 @@ export default function Page() {
         <TextGrid content={howWeDoItCopy} />
         <Testimonials11 />
         <TextBlockDark content={reviewsCtaCopy} />
-        <FAQ content={faqCopy} />
+        <SectionTitle
+          title="The Knowledge Center"
+          subtitle="Where you actually learn new things"
+        />
         <BlogHighlight articles={blogHighlightCopy} />
       </main>
     </>
