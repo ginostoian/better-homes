5862526641482a4ee2c64ddde6c26d14013658ab
Gino Stoian <ginostoian@gmail.com>
2024-05-01 11:07:36 +0300

    contact form

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,7 +4,6 @@ import Problem from "@/components/Problem";
 import Testimonials11 from "@/components/Testimonials11";
 import WithWithout from "@/components/WithWithout";
 import BlogHighlight from "@/components/blog/BlogHighlight";
-import ContentRow from "@/components/contentRow/ContentRow";
 import Hero from "@/components/hero/Hero";
 import SectionTitle from "@/components/sectionTitle/SectionTitle";
 import SocialProof from "@/components/socialProof/SocialProof";
@@ -13,54 +12,46 @@ import TextGrid from "@/components/textGrid/TextGrid";
 import config from "@/config";
 
 export default function Page() {
-  const homepageCopy = config.copy.homepage;
-  const whatWeDoCopy = config.copy.homepage.whatWeDoSection;
-  const whatWeDoArr = [
-    whatWeDoCopy.bathroom,
-    whatWeDoCopy.kitchen,
-    whatWeDoCopy.generalRenovation,
-    whatWeDoCopy.interiorDesign,
-    whatWeDoCopy.houseExtension,
-    whatWeDoCopy.smartHome,
-  ];
-  const howWeDoItCopy = config.copy.homepage.howWeDoItSection;
+  const aboutPageCopy = config.copy.aboutPage;
+  //   const whatWeDoCopy = aboutPageCopy.whatWeDoSection;
+  //   const whatWeDoArr = [
+  //     whatWeDoCopy.fullHome,
+  //     whatWeDoCopy.bathroomInstallation,
+  //     whatWeDoCopy.kitchenInstallation,
+  //     whatWeDoCopy.structuralWork,
+  //     whatWeDoCopy.heating,
+  //     whatWeDoCopy.flooringInstallation,
+  //   ];
+  const howWeDoItCopy = aboutPageCopy.howWeDoItSection;
   const reviewsCtaCopy = config.copy.homepage.reviewsSection;
-  const faqCopy = config.copy.homepage.faqs;
-  const blogHighlightCopy = config.copy.homepage.blogHighlights;
+  const faqCopy = aboutPageCopy.faqs;
+  const blogHighlightCopy = aboutPageCopy.blogHighlights;
 
   return (
     <>
       <main>
         <Hero
-          title={homepageCopy.title}
-          titleAccent={homepageCopy.titleAccent}
-          subtitle={homepageCopy.subtitle}
-          heroCTA={homepageCopy.heroCTA}
-          heroImgUrl={homepageCopy.heroImgUrl}
+          title={aboutPageCopy.title}
+          titleAccent={aboutPageCopy.titleAccent}
+          subtitle={aboutPageCopy.subtitle}
+          heroCTA={aboutPageCopy.heroCTA}
+          heroImgUrl={aboutPageCopy.heroImgUrl}
+          ctaTallyFormLink={aboutPageCopy.ctaTallyFormLink}
         />
         <SocialProof />
+        <iframe
+          data-tally-src="https://tally.so/embed/wQEoXw?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+          width="100%"
+          height="300"
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
+          title="Contact us"
+          className="contact-iframe"
+          id="contactForm"
+        ></iframe>
         <Problem />
         <FeaturesListicle />
-        <SectionTitle
-          title="What we do"
-          subtitle="Full service renovation company"
-        />
-        {whatWeDoArr.map((item, i) => {
-          return (
-            <ContentRow
-              key={i}
-              title={item.title}
-              subtitle={item.subtitle}
-              subtitleAccent={item.subtitleAccent}
-              p1={item.p1}
-              p2={item.p2}
-              cta={item.CTA}
-              imgSrc={item.imgSrc}
-              order={i % 2 === 0 ? false : true}
-              slug={item.slug}
-            />
-          );
-        })}
         <WithWithout />
         <SectionTitle
           title="How we do it"
@@ -70,6 +61,10 @@ export default function Page() {
         <Testimonials11 />
         <TextBlockDark content={reviewsCtaCopy} />
         <FAQ content={faqCopy} />
+        <SectionTitle
+          title="The Knowledge Center"
+          subtitle="Where you actually learn new things"
+        />
         <BlogHighlight articles={blogHighlightCopy} />
       </main>
     </>
